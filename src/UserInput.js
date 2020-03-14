4620bc90aa38549c90df209e73715137d0de6b7c
Javier Reyes <jreyesribes@gmail.com>
2020-03-14 19:14:02 +0100

    UserInput & UserOutput EXIST. multiple outputs and one input get shown.

---
+++ src/UserInput.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class UserInput extends Component {
+  render() {
+    return (
+      <div className = "UserInput">
+        <input type = "text" />
+      </div>
+    );
+  }
+}
+
+export default UserInput;
---
+++ src/UserOutput.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class UserOutput extends Component {
+  render() {
+    return (
+      <div className = "UserOutput">
+        <p>Hey!</p>
+        <p>Sup, dude.</p>
+      </div>
+    );
+  }
+}
+
+export default UserOutput;
