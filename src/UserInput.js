e1d07eb71e4a1e518963f31a10a0c8d97d900b4e
Javier Reyes <jreyesribes@gmail.com>
2020-03-14 20:06:13 +0100

    styles

--- src/UserInput.js
+++ src/UserInput.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import './UserInput.css';
 
 class UserInput extends Component {
   render() {
--- src/UserOutput.js
+++ src/UserOutput.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import './UserOutput.css';
 
 class UserOutput extends Component {
   render() {
